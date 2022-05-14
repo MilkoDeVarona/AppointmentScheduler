84659728f0f8d2d2b7b964c8ba23f1bc5f773476
MilkoDeVarona <mvaron1@wgu.edu>
2022-05-14 01:53:13 -0400

    second save

---
+++ src/controller/Login.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Login implements Initializable {
+
+    public Label LoginLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // System.out.println("I am initialized");
+        LoginLabel.setText("I am the label!");
+    }
+
+    public void onSubmitButtonAction(ActionEvent actionEvent) {
+        // System.out.println("This button was clicked");
+        LoginLabel.setText("Click clickity click");
+    }
+
+}
